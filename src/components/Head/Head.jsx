367717dba86ac1a2c20192b12792014c050ff2b5
goyadev <goyazheng@gmail.com>
2024-02-13 12:53:24 +0800

    added header and av with responsive design

---
+++ src/components/Head/Head.jsx
@@ -0,0 +1,26 @@
+import "./style.css";
+
+export function Head() {
+  return (
+    <header>
+      <h1 id="headingname">Goya Zheng</h1>
+
+      <a href="https://www.linkedin.com/in/goya-zheng-615b54111/" id="linkedin">
+        <img
+          src="../../public/In-White-72.png"
+          class="social-logos"
+          alt="linkedin
+        logo in white"
+        />
+      </a>
+
+      <a href="https://github.com/goyadev" id="github">
+        <img
+          src="../../public/github-mark-white.png"
+          class="social-logos"
+          alt="github logo in white"
+        />
+      </a>
+    </header>
+  );
+}
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,38 @@
+import "./style.css";
+
+export function NavBar() {
+  return (
+    <nav>
+      <div class="navbar">
+        <div class="container nav-container">
+          <input
+            class="checkbox"
+            type="checkbox"
+            name="hamburgercheckbox"
+            id="checkboxburger"
+          />
+          <div class="hamburger-lines">
+            <span class="line line1"></span>
+            <span class="line line2"></span>
+            <span class="line line3"></span>
+          </div>
+
+          <ul class="menu-items">
+            <li>
+              <a href="./index.html">Home</a>
+            </li>
+            <li>
+              <a href="./bio.html">Bio</a>
+            </li>
+            <li>
+              <a href="./projects.html">Projects</a>
+            </li>
+            <li>
+              <a href="./contact.html">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
